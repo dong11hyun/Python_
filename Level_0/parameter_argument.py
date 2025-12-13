95d43d3ef9dd0ddd45b99130f046ff3de8b2da44
dong11hyun <eastmanin@gmail.com>
2025-12-13 21:17:32 +0900

    25_12_13

---
+++ Level_0/parameter_argument.py
@@ -0,0 +1,10 @@
+def travel(country: str, name: str):
+    print(f" hello {country}")
+    print(f" goodbye {name}")
+travel('1','lol')
+
+
+# 파라미터 (매개변수): 함수를 **정의(Definition)**할 때 사용되는 변수 이름 
+
+# 아규먼트 (인자/인수): 함수를 **호출(Call)**할 때 실제로 전달하는 값 (Real Value)
+# 함수 쓸때 파라미터 = 아규먼트 형식으로 확실히 정의해서 적어주면 좋음.
